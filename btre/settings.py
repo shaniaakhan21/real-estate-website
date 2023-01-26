e2a01ddff3c5933ef1b395bf77ab7ce65b9e7fec
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2023-01-26 21:45:51 +0400

    commit

--- btre/settings.py
+++ btre/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'hczb7l8j&5c^g_222d1!(n0vlb@==lnt89*&ab6iia-fc!!jna'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['54.198.27.123']
 
 
 # Application definition
