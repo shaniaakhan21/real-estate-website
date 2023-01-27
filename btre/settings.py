e1ecfaa6338d454daed0a936e6111e2587c63e19
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2023-01-27 17:12:06 +0400

    17:11

--- btre/settings.py
+++ btre/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,6 +73,8 @@ TEMPLATES = [
         },
     },
 ]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 WSGI_APPLICATION = 'btre.wsgi.application'
 
