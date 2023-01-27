f817dc9baf267b1d033792af5a5c8bb85077561b
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2023-01-27 19:31:09 +0400

    19:31

--- btre/urls.py
+++ btre/urls.py
@@ -1,15 +1,13 @@
-from django.contrib.admin import site
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.decorators.csrf import csrf_exempt
 
-    
 urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
     path('contacts/', include('contacts.urls')),
     path('accounts/', include('accounts.urls')),
-    path('admin/', csrf_exempt(site.urls)),
+    path('admin/', admin.site.urls),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
