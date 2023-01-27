7180e36d124cad044afd92e4094a0156b33ec009
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2023-01-27 16:57:55 +0400

    railway once more

--- btre/settings.py
+++ btre/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hczb7l8j&5c^g_222d1!(n0vlb@==lnt89*&ab6iia-fc!!jna'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
